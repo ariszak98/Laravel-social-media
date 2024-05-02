21355c4af62416b5af9c2c834bc3f592986ca71f
ariszak98 <ariszak98@icloud.com>
2024-05-02 05:39:00 +0300

    Modified the Routes file (routes/web.php)

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "<h1>Home Page</h1>
+    <hr>
+    <p>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem<br>
+    lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum 
+    lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
+    <hr>
+    <a href='/about'>Goto About page</a>";
+    #return view('welcome');
+});
+
+Route::get('/about', function () {
+    return "<h1>About Page</h1>
+    <hr>
+    <p>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem<br>
+    lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum 
+    lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
+    <hr>
+    <a href='/'>Back to Home</a>";
 });
